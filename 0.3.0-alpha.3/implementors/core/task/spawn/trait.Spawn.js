376cf8cb215bfe1fae6dea663386fb819001ec27
Josef Reinhard Brandl <mail@josefbrandl.de>
2018-08-15 02:32:37 +0200

    alpha 3 docs

---
+++ 0.3.0-alpha.3/implementors/core/task/spawn/trait.Spawn.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["futures_executor"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/task/spawn/trait.Spawn.html\" title=\"trait core::task::spawn::Spawn\">Spawn</a> for <a class=\"struct\" href=\"futures_executor/struct.LocalSpawn.html\" title=\"struct futures_executor::LocalSpawn\">LocalSpawn</a>",synthetic:false,types:["futures_executor::local_pool::LocalSpawn"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/task/spawn/trait.Spawn.html\" title=\"trait core::task::spawn::Spawn\">Spawn</a> for <a class=\"struct\" href=\"futures_executor/struct.ThreadPool.html\" title=\"struct futures_executor::ThreadPool\">ThreadPool</a>",synthetic:false,types:["futures_executor::thread_pool::ThreadPool"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
