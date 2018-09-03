4adcdb28f1e0a7a18fb15f6b3bb203e3c8c09482
Josef Reinhard Brandl <mail@josefbrandl.de>
2018-09-03 23:10:43 +0200

    0.3.0-alpha.5 docs

---
+++ 0.3.0-alpha.5/implementors/core/task/wake/trait.UnsafeWake.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["futures_test"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/task/wake/trait.UnsafeWake.html\" title=\"trait core::task::wake::UnsafeWake\">UnsafeWake</a> for <a class=\"struct\" href=\"futures_test/task/struct.NoopWake.html\" title=\"struct futures_test::task::NoopWake\">NoopWake</a>",synthetic:false,types:["futures_test::task::noop_waker::NoopWake"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/task/wake/trait.UnsafeWake.html\" title=\"trait core::task::wake::UnsafeWake\">UnsafeWake</a> for <a class=\"struct\" href=\"futures_test/task/struct.PanicWake.html\" title=\"struct futures_test::task::PanicWake\">PanicWake</a>",synthetic:false,types:["futures_test::task::panic_waker::PanicWake"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
