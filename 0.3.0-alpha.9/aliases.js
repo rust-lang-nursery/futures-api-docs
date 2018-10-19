12853196f474e08e7ed042c3314b1901a7102078
Travis CI User <travis@example.org>
2018-10-19 03:16:32 +0000

    Add API docs for 0.3.0-alpha.9

---
+++ 0.3.0-alpha.9/aliases.js
@@ -0,0 +1,9 @@
+var ALIASES = {};
+ALIASES['futures'] = {};
+ALIASES['futures_channel'] = {};
+ALIASES['futures_core'] = {};
+ALIASES['futures_executor'] = {};
+ALIASES['futures_io'] = {};
+ALIASES['futures_sink'] = {};
+ALIASES['futures_test'] = {};
+ALIASES['futures_util'] = {};
---
+++ 0.3.0-alpha.9/implementors/core/hash/trait.Hash.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["futures_util"] = [{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"futures_util/io/struct.AllowStdIo.html\" title=\"struct futures_util::io::AllowStdIo\">AllowStdIo</a>&lt;T&gt;",synthetic:false,types:["futures_util::io::allow_std::AllowStdIo"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
