f863f6e8fb14242cefb7836373f1c3750511f98b
Travis CI User <travis@example.org>
2018-11-27 21:01:15 +0000

    Add API docs for 0.3.0-alpha.10

---
+++ 0.3.0-alpha.10/aliases.js
@@ -0,0 +1,17 @@
+var ALIASES = {};
+ALIASES['futures'] = {};
+ALIASES['futures'] = {};
+ALIASES['futures_channel'] = {};
+ALIASES['futures_channel'] = {};
+ALIASES['futures_core'] = {};
+ALIASES['futures_core'] = {};
+ALIASES['futures_executor'] = {};
+ALIASES['futures_executor'] = {};
+ALIASES['futures_io'] = {};
+ALIASES['futures_io'] = {};
+ALIASES['futures_sink'] = {};
+ALIASES['futures_sink'] = {};
+ALIASES['futures_test'] = {};
+ALIASES['futures_test'] = {};
+ALIASES['futures_util'] = {};
+ALIASES['futures_util'] = {};
