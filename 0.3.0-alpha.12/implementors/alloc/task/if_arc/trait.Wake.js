b97aaf0ba38a46e2123ce972138a1bbeef35b3a7
Travis CI User <travis@example.org>
2019-01-15 02:03:50 +0000

    Add API docs for 0.3.0-alpha.12

---
+++ 0.3.0-alpha.12/implementors/alloc/task/if_arc/trait.Wake.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["futures_test"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/task/if_arc/trait.Wake.html\" title=\"trait alloc::task::if_arc::Wake\">Wake</a> for <a class=\"struct\" href=\"futures_test/task/struct.PanicWake.html\" title=\"struct futures_test::task::PanicWake\">PanicWake</a>",synthetic:false,types:["futures_test::task::panic_waker::PanicWake"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
