3633fb49bb06937385dd42a883b816d3ca3073f2
Travis CI User <travis@example.org>
2019-04-27 00:43:54 +0000

    Add API docs for 0.3.0-alpha.15

---
+++ 0.3.0-alpha.15/implementors/futures_util/io/trait.AsyncSeek.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["futures_util"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"futures_util/io/trait.AsyncSeek.html\" title=\"trait futures_util::io::AsyncSeek\">AsyncSeek</a> for <a class=\"struct\" href=\"futures_util/io/struct.AllowStdIo.html\" title=\"struct futures_util::io::AllowStdIo\">AllowStdIo</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Seek.html\" title=\"trait std::io::Seek\">Seek</a>,&nbsp;</span>",synthetic:false,types:["futures_util::io::allow_std::AllowStdIo"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
